967798516a0a611e1d36cdf44a4b7546cb675a4d
Tej <gurtej1992@gmail.com>
2024-05-11 17:48:26 -0400

    Initial Commit

---
+++ NewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by Gurtej Singh on 2024-05-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
